b5771f489bcd34f773ca56984037cfd2e1a5e4ed
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-02-22 21:11:13 +0000

    add condition statements to return win/lose result

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,18 +4,32 @@ function getComputerChoice(min, max) {
 
 
 if (getComputerChoice(1, 3) === 1) {
-    answer = "rock";
+    computerSelection = "rock";
 } else if (getComputerChoice(1, 3) === 2) {
-    answer = "paper";
+    computerSelection = "paper";
 } else {
-    answer = "scissors";
+    computerSelection = "scissors";
 }
 
-const playerSelection = "rock"; //player selection will come from text input
-const computerSelection = getComputerChoice();
-console.log(playerSelection, answer);
+
+let playerSelection = "rock"; //player selection will come from text input
 
 
+if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+} else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("Paper beats rock! Computer wins!");
+} else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log("Rock beats scissors! You win!");
+} else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log("Paper beats rock! You win!");
+} else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("Scissors beat paper! Computer wins!");
+} else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log("Scissors beat paper! You win!");
+} else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("Rock beats scissors! Computer wins!");
+}
 
 
 
