e594a5e5a896cbd37aac30ddad3dccb90341ec70
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-02-22 20:35:16 +0000

    create variables for player/computer selection print them together in console

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,20 @@
-function getRandomNumber(min, max) {
+function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
-if (getRandomNumber(1, 3) === 1) {
-    console.log("rock");
-} else if (getRandomNumber(1, 3) === 2) {
-    console.log("paper");
-} else if (getRandomNumber(1, 3) === 3) {
-    console.log("scissors");
+if (getComputerChoice(1, 3) === 1) {
+    answer = "rock";
+} else if (getComputerChoice(1, 3) === 2) {
+    answer = "paper";
+} else {
+    answer = "scissors";
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playerSelection, answer);
+
 
 
 
