f9533e49068d89a5a831e46f43a2bc869ec9b403
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-02-22 21:16:49 +0000

    create playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -15,21 +15,25 @@ if (getComputerChoice(1, 3) === 1) {
 let playerSelection = "rock"; //player selection will come from text input
 
 
-if (playerSelection === computerSelection) {
-    console.log("It's a tie!");
-} else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("Paper beats rock! Computer wins!");
-} else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log("Rock beats scissors! You win!");
-} else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("Paper beats rock! You win!");
-} else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("Scissors beat paper! Computer wins!");
-} else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("Scissors beat paper! You win!");
-} else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("Rock beats scissors! Computer wins!");
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Paper beats rock! Computer wins!");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("Rock beats scissors! You win!");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Paper beats rock! You win!");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("Scissors beat paper! Computer wins!");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("Scissors beat paper! You win!");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("Rock beats scissors! Computer wins!")
+    }
+
 }
 
+playRound(playerSelection, computerSelection);
 
 
