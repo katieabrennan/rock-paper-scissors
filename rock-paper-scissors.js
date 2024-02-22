81939af3a0ca28731a0b1c8bcd205bf7cf3164a3
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-02-22 16:19:17 +0000

    Convert random number into rock, paper or scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,8 +2,14 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(getRandomNumber(1, 3));
 
+if (getRandomNumber(1, 3) === 1) {
+    console.log("rock");
+} else if (getRandomNumber(1, 3) === 2) {
+    console.log("paper");
+} else if (getRandomNumber(1, 3) === 3) {
+    console.log("scissors");
+}
 
 
 
