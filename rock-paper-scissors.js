40b0e74ab59bf0a055d26e4c8c7f262796b6422d
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-02-23 14:35:28 +0000

    include overall winner after x5 games

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,29 +10,70 @@ if (getComputerChoice(1, 3) === 1) {
     computerSelection = "scissors";
 }
 
-let playerSelection = prompt("rock, paper or scissors?");
-
+let compScore = 0
+let playerScore = 0
 
 
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection.toLowerCase() === computerSelection) {
-        console.log("It's a tie!");
+        console.log("It's a tie! You both selected " + playerSelection);
+        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-        console.log("Paper beats rock! Computer wins!");
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+        compScore++
+        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-        console.log("Rock beats scissors! You win!");
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+        playerScore++
+        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-        console.log("Paper beats rock! You win!");
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+        playerScore++
+        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-        console.log("Scissors beat paper! Computer wins!");
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+        compScore++
+        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-        console.log("Scissors beat paper! You win!");
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+        playerScore++
+        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        console.log("Rock beats scissors! Computer wins!")
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+        compScore++
+        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
     }
 
 }
 
+let playerSelection = prompt("rock, paper or scissors?");
 playRound(playerSelection, computerSelection);
+
+playerSelection = prompt("rock, paper or scissors?");
+playRound(playerSelection, computerSelection)
+
+playerSelection = prompt("rock, paper or scissors?");
+playRound(playerSelection, computerSelection)
+
+playerSelection = prompt("rock, paper or scissors?");
+playRound(playerSelection, computerSelection)
+
+playerSelection = prompt("rock, paper or scissors?");
+playRound(playerSelection, computerSelection)
+
+if (compScore > playerScore) {
+    console.log("YOU LOST");
+} else if (compScore < playerScore) {
+    console.log("YOU WON!");
+} else if (compScore === playerScore) {
+    console.log("IT'S A TIE!")
+}
+
+
+
+
+
+
 
 
