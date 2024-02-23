25ae88abd51a9121d23c16b48a8d9e9aa4cd4e46
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-02-23 09:51:03 +0000

    always convert user input to lower case code wouldn't run if user had capitalised input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,6 @@ function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 if (getComputerChoice(1, 3) === 1) {
     computerSelection = "rock";
 } else if (getComputerChoice(1, 3) === 2) {
@@ -11,24 +10,24 @@ if (getComputerChoice(1, 3) === 1) {
     computerSelection = "scissors";
 }
 
-
 let playerSelection = prompt("rock, paper or scissors?");
 
 
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         console.log("It's a tie!");
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
         console.log("Paper beats rock! Computer wins!");
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
         console.log("Rock beats scissors! You win!");
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
         console.log("Paper beats rock! You win!");
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
         console.log("Scissors beat paper! Computer wins!");
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
         console.log("Scissors beat paper! You win!");
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
         console.log("Rock beats scissors! Computer wins!")
     }
 
