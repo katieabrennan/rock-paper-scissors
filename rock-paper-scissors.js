1fd1d77b2933587fff7f1f86e25e7b185c439fca
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-03-20 10:38:54 +0000

    add a div to display results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,38 +13,38 @@ if (getComputerChoice(1, 3) === 1) {
 let compScore = 0
 let playerScore = 0
 
-const results = document.querySelector("div");
+const results = document.querySelector("#results");
 
 
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection.toLowerCase() === computerSelection) {
-        console.log("It's a tie! You both selected " + playerSelection);
-        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
+        results.textContent = `It's a tie! You both selected ${playerSelection} 
+        You: ${playerScore} - Computer: ${compScore}`;
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-        console.log("You lose! " + computerSelection + " beats " + playerSelection);
         compScore++
-        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}
+        You: ${playerScore} - Computer: ${compScore}`;
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-        console.log("You win! " + playerSelection + " beats " + computerSelection);
         playerScore++
-        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}
+        You: ${playerScore} - Computer: ${compScore}`;
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-        console.log("You win! " + playerSelection + " beats " + computerSelection);
         playerScore++
-        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}
+        You: ${playerScore} - Computer: ${compScore}`;
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-        console.log("You lose! " + computerSelection + " beats " + playerSelection);
         compScore++
-        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection};
+        You: ${playerScore} - Computer: ${compScore}`;
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-        console.log("You win! " + playerSelection + " beats " + computerSelection);
         playerScore++
-        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}
+        You: ${playerScore} - Computer: ${compScore}`;
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        console.log("You lose! " + computerSelection + " beats " + playerSelection);
         compScore++
-        console.log("You: " + playerScore + " - " + "Computer: " + compScore);
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}
+        You: ${playerScore} - Computer: ${compScore}`;
     }
 }
 
