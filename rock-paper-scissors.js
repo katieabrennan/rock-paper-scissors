5113849c2ba0ddd8c0e8dff13c5dcc8e53627e85
“Katie” <152707851+katieabrennan@users.noreply.github.com>
2024-03-20 11:17:25 +0000

    bring computer selection within playRound function computer's choice was the same every round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,14 +2,6 @@ function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-if (getComputerChoice(1, 3) === 1) {
-    computerSelection = "rock";
-} else if (getComputerChoice(1, 3) === 2) {
-    computerSelection = "paper";
-} else {
-    computerSelection = "scissors";
-}
-
 let compScore = 0
 let playerScore = 0
 
@@ -18,83 +10,66 @@ const results = document.querySelector("#results");
 
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection.toLowerCase() === computerSelection) {
-        results.textContent = `It's a tie! You both selected ${playerSelection} 
-        You: ${playerScore} - Computer: ${compScore}`;
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-        compScore++
-        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}
-        You: ${playerScore} - Computer: ${compScore}`;
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-        playerScore++
-        results.textContent = `You win! ${playerSelection} beats ${computerSelection}
-        You: ${playerScore} - Computer: ${compScore}`;
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-        playerScore++
-        results.textContent = `You win! ${playerSelection} beats ${computerSelection}
-        You: ${playerScore} - Computer: ${compScore}`;
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-        compScore++
-        results.textContent = `You lose! ${computerSelection} beats ${playerSelection};
-        You: ${playerScore} - Computer: ${compScore}`;
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-        playerScore++
-        results.textContent = `You win! ${playerSelection} beats ${computerSelection}
-        You: ${playerScore} - Computer: ${compScore}`;
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        compScore++
-        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}
-        You: ${playerScore} - Computer: ${compScore}`;
+    if (playerSelection.toLowerCase() === "rock") {
+
+        if (computerSelection === 1) {
+            results.textContent = `It's a tie! You both selected ${playerSelection} 
+            You: ${playerScore} - Computer: ${compScore}`;
+        } else if (computerSelection === 2) {
+            compScore++
+            results.textContent = `You lose! ${computerSelection} beats ${playerSelection}
+            You: ${playerScore} - Computer: ${compScore}`;
+        } else {
+            playerScore++
+            results.textContent = `You win! ${playerSelection} beats ${computerSelection}
+            You: ${playerScore} - Computer: ${compScore}`;
+        }
+
+    } else if (playerSelection.toLowerCase() === "paper") {
+
+        if (computerSelection === 1) {
+            playerScore++
+            results.textContent = `You win! ${playerSelection} beats ${computerSelection}
+            You: ${playerScore} - Computer: ${compScore}`;
+        } else if (computerSelection === 2) {
+            results.textContent = `It's a tie! You both selected ${playerSelection} 
+            You: ${playerScore} - Computer: ${compScore}`;
+        } else {
+            compScore++
+            results.textContent = `You lose! ${computerSelection} beats ${playerSelection};
+            You: ${playerScore} - Computer: ${compScore}`;
+        }
+
+    } else if (playerSelection.toLowerCase() === "scissors") {
+
+        if (computerSelection === 1) {
+            compScore++
+            results.textContent = `You lose! ${computerSelection} beats ${playerSelection}
+            You: ${playerScore} - Computer: ${compScore}`;
+        } else if (computerSelection === 2) {
+            playerScore++
+            results.textContent = `You win! ${playerSelection} beats ${computerSelection}
+            You: ${playerScore} - Computer: ${compScore}`;
+        } else {
+            results.textContent = `It's a tie! You both selected ${playerSelection} 
+            You: ${playerScore} - Computer: ${compScore}`;
+        }
     }
+
 }
 
-// let playerSelection = prompt("rock, paper or scissors?");
-// playRound(playerSelection, computerSelection);
-
-// playerSelection = prompt("rock, paper or scissors?");
-// playRound(playerSelection, computerSelection)
-
-// playerSelection = prompt("rock, paper or scissors?");
-// playRound(playerSelection, computerSelection)
-
-// playerSelection = prompt("rock, paper or scissors?");
-// playRound(playerSelection, computerSelection)
-
-// playerSelection = prompt("rock, paper or scissors?");
-// playRound(playerSelection, computerSelection)
-
-// if (compScore > playerScore) {
-//     console.log("YOU LOST");
-// } else if (compScore < playerScore) {
-//     console.log("YOU WON!");
-// } else if (compScore === playerScore) {
-//     console.log("IT'S A TIE!")
-// }
 
 const btnRock = document.querySelector("#rock");
 btnRock.addEventListener("click", () => {
-    playRound("rock", computerSelection);
+    playRound("rock", getComputerChoice(1, 3));
 });
 
 const btnPaper = document.querySelector("#paper");
 btnPaper.addEventListener("click", () => {
-    playRound("paper", computerSelection);
+    playRound("paper", getComputerChoice(1, 3));
 });
 
 const btnScissors = document.querySelector("#scissors");
 btnScissors.addEventListener("click", () => {
-    playRound("scissors", computerSelection);
+    playRound("scissors", getComputerChoice(1, 3));
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
